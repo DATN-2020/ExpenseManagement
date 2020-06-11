79d9e13c78e3d004c90c35c5b73453772367f449
Hoang Phuc <hphuc5453@gmail.com>
2020-06-11 21:37:51 +0700

    - Update api type category

--- RESOURCES/DATN_ExpanseManagement/app/src/main/java/datn/datn_expansemanagement/screen/list_type_category/domain/ListTypeCategoryMapper.kt
+++ RESOURCES/DATN_ExpanseManagement/app/src/main/java/datn/datn_expansemanagement/screen/list_type_category/domain/ListTypeCategoryMapper.kt
@@ -7,33 +7,47 @@ import datn.datn_expansemanagement.kotlinex.number.getValueOrDefaultIsZero
 import datn.datn_expansemanagement.kotlinex.string.getValueOrDefaultIsEmpty
 import datn.datn_expansemanagement.screen.list_type_category.presentation.model.TypeCategoryItemViewModel
 
-class ListTypeCategoryMapper(private val typeId: Int? = null) : Mapper<List<TypeCategoryResponse>, MutableList<ViewModel>>{
+class ListTypeCategoryMapper(private val typeId: Int? = null) :
+    Mapper<List<TypeCategoryResponse>, MutableList<ViewModel>> {
     override fun map(input: List<TypeCategoryResponse>): MutableList<ViewModel> {
         val list = mutableListOf<ViewModel>()
+        var i = 0
 
-        if(!input.isNullOrEmpty()){
+        if (!input.isNullOrEmpty()) {
             input.forEach {
-                list.add(TypeCategoryItemViewModel(
-                    id = it.idType.getValueOrDefaultIsZero(),
-                    name = it.name.getValueOrDefaultIsEmpty(),
-                    imageUrl = it.image.getValueOrDefaultIsEmpty(),
-                    isLast = true,
-                    isChoose = false
-                ))
+                if (i == input.size - 1) {
+                    list.add(
+                        TypeCategoryItemViewModel(
+                            id = it.idType.getValueOrDefaultIsZero(),
+                            name = it.name.getValueOrDefaultIsEmpty(),
+                            imageUrl = it.image.getValueOrDefaultIsEmpty(),
+                            isLast = true
+                        )
+                    )
+                } else {
+                    list.add(
+                        TypeCategoryItemViewModel(
+                            id = it.idType.getValueOrDefaultIsZero(),
+                            name = it.name.getValueOrDefaultIsEmpty(),
+                            imageUrl = it.image.getValueOrDefaultIsEmpty(),
+                            isChoose = false
+                        )
+                    )
+                }
+
             }
+            i++
         }
 
-
-        if(typeId != null){
+        if (typeId != null) {
             list.forEach {
-                if(it is TypeCategoryItemViewModel){
-                    if(it.id == typeId){
+                if (it is TypeCategoryItemViewModel) {
+                    if (it.id == typeId) {
                         it.isChoose = true
                     }
                 }
             }
         }
-
         return list
     }
 
